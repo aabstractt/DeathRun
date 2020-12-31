5bcee5e56a569262060662cda32198e9c6ce05d2
iTheTrollIdk <xavier04.martinez@gmail.com>
2020-12-30 20:52:24 -0600

    [Build]

--- src/deathrun/listener/PlayerListener.php
+++ src/deathrun/listener/PlayerListener.php
@@ -36,8 +36,6 @@ class PlayerListener implements Listener {
 
         if ($player == null) return;
 
-        if ($player->isRunner()) return;
-
         $item = $ev->getItem();
 
         $nbt = $item->getCustomBlockData();
@@ -50,26 +48,30 @@ class PlayerListener implements Listener {
 
         if ($name == null) return;
 
-        if ($name == 'Last' || $name == 'Next') {
-            $name == 'Last' ? $player->decrease() : $player->increase();
-
-            $player->executeTeleport();
-
-            return;
-        }
-
         if ($name == 'Leap') {
             if ($player->hasLeapCountDown()) return;
 
             $direction = $player->getGeneralPlayer()->getDirectionVector();
 
-            $player->getGeneralPlayer()->knockBack($player->getGeneralPlayer(), 0, $direction->getFloorX(), $direction->getFloorZ());
+            $player->getGeneralPlayer()->knockBack($player->getGeneralPlayer(), 0, $direction->getFloorX(), $direction->getFloorZ(), 5);
+
+            $player->sendMessage('Cool');
 
             $player->setTrapCountDown(true);
 
             DeathRun::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use($player): void {
                 $player->setLeapCountDown();
             }), 20 * 30);
+            return;
+        }
+
+        if ($player->isRunner()) return;
+
+        if ($name == 'Last' || $name == 'Next') {
+            $name == 'Last' ? $player->decrease() : $player->increase();
+
+            $player->executeTeleport();
+
             return;
         }
 
